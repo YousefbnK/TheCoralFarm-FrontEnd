9789c582dc05d15eb6c85a7eda357288251b578c
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-03-11 20:08:03 +0300

    Created Cart page, cart store

---
+++ components/Cart/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { observer } from "mobx-react";
+
+// NativeBase Components
+import { Text, List, Button } from "native-base";
+
+// Component
+import CartItem from "./CartItem";
+
+// Stores
+import cartStore from "../../stores/CartStore";
+
+const CoralCart = () => {
+  const cartItems = cartStore.items.map(item => (
+    <CartItem item={item} key={`${item.id}`} />
+  ));
+
+  return (
+    <List>
+      {cartItems}
+      <Button full danger>
+        <Text>Checkout</Text>
+      </Button>
+    </List>
+  );
+};
+
+// CoffeeCart.navigationOptions = {
+//   title: "Cart"
+// };
+
+export default observer(CoralCart);
