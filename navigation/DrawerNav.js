418b1c0390df14b01c75f67cc48575a6b639b020
Yousef Alkhalifah <y.alkhalifah@hotmail.com>
2020-03-21 20:44:39 +0300

    Design elements for drawerNav

--- navigation/DrawerNav.js
+++ navigation/DrawerNav.js
@@ -1,23 +1,37 @@
-import { createDrawerNavigator } from "react-navigation-drawer";
+import React from "react";
+import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
+import { Text, View } from "react-native";
 
 //Components
 import Profile from "../components/Profile";
+import OrderList from "../components/Profile/OrderList";
+import StackNav from "./StackNav";
 
 //Navs
 
-import CoralList from "../components/CoralList";
+const DrawerContent = props => (
+  <View>
+    <View
+      style={{
+        backgroundColor: "#00BFFF",
+        height: 140,
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <Text style={{ color: "white", fontSize: 30 }}>Hello</Text>
+    </View>
+    <DrawerItems {...props} />
+  </View>
+);
 
 const DrawerNav = createDrawerNavigator(
   {
-    ListScreen: CoralList,
-    Profile: Profile
+    Home: { screen: StackNav },
+    Profile: { screen: Profile },
+    Orders: { screen: OrderList }
   },
-  {
-    initialRouteName: "Profile",
-    defaultNavigationOptions: {
-      title: "Profile"
-    }
-  }
+  { contentComponent: DrawerContent }
 );
 
 export default DrawerNav;
